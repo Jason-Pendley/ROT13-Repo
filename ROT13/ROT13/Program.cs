203cce2a1692204b82d4b1b62b181265524722db
Jason Pendley <Jason.R.Pendley-1@ou.edu>
2020-08-18 16:30:00 -0500

    Up and working.

---
+++ ROT13/ROT13/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ROT13
+{
+    class Program
+    {
+        public static char Rot(char c)
+        {
+            if ('a' <= c && c <= 'm' || 'A' <= c && c <= 'M')
+            {
+                return (char)(c + 13);
+            }
+            if ('n' <= c && c <= 'z' || 'N' <= c && c <= 'Z')
+            {
+                return (char)(c - 13);
+            }
+            return c;
+        }
+        public static string En(string input)
+        {
+            string output = string.Empty;
+
+            foreach (char ch in input)
+                output += Rot(ch);
+            return output;
+        }
+        //public static string dE(string input)
+        //{
+        //    return En(input - 13);
+        //}  
+    static void Main(string[] args)
+        {
+            WriteLine("Please enter text for ROT!");
+            string userin = ReadLine();
+            WriteLine("\n");
+
+            WriteLine("The encrypted message is:");
+            string em = En(userin);
+            WriteLine(em);
+            WriteLine("\n");
+
+            WriteLine("Enter 1 to show original message!");
+            double uM = Convert.ToInt32(ReadLine());
+            
+            if(uM == 1)
+            {
+                WriteLine("Your decrypted message is:");
+                string dm = userin;
+                WriteLine(dm);
+                WriteLine("\n");
+            }
+
+            ReadKey();
+        }
+    }
+}
